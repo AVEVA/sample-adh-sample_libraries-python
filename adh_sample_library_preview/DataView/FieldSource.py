d02058562d2c4596be521d290400a26b597c4e89
fabiodani5450 <fdani@osisoft.com>
2023-03-24 15:27:15 -0400

    Added new enumeration set for dataViews
    After communities we needed to add new fields for the DataFields.

--- adh_sample_library_preview/DataView/FieldSource.py
+++ adh_sample_library_preview/DataView/FieldSource.py
@@ -12,3 +12,9 @@ class FieldSource(Enum):
     PropertyName = 4
     Metadata = 5
     Tags = 6
+    TenantId = 7
+    TenantName = 8
+    NamespaceId = 9
+    NamespaceName = 10
+    CommunityId = 11
+    CommunityName = 12
