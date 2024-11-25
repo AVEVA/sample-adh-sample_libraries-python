6d37809f3d9ce955acee8d795c1047f477c2e94e
Adam Fink <adam.fink@aveva.com>
2024-11-25 09:52:48 -0500

    Consistency changes, update all routes

--- adh_sample_library_preview/Signups.py
+++ adh_sample_library_preview/Signups.py
@@ -34,12 +34,11 @@ class Signups(object):
         """
 
         self.__base_client.validateParameters(namespace_id, community_id)
-
-        if community_id is not None:
-            communityHeader = self.__base_client.getCommunityIdHeader(community_id)
+        
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         response = self.__base_client.request('get', self.__signups_path.format(
-            namespace_id=namespace_id), additional_headers=communityHeader)
+            namespace_id=namespace_id), additional_headers=additional_headers)
         self.__base_client.checkResponse(
             response, f'Failed to get signups.')
 
@@ -62,14 +61,13 @@ class Signups(object):
 
         self.__base_client.validateParameters(namespace_id, community_id)
 
-        if community_id is not None:
-            communityHeader = self.__base_client.getCommunityIdHeader(community_id)
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         if not isinstance(body, CreateSignupInput):
             raise TypeError
 
         response = self.__base_client.request('post', self.__signups_path.format(
-            namespace_id=namespace_id), data=body.toJson(), additional_headers=communityHeader)
+            namespace_id=namespace_id), data=body.toJson(), additional_headers=additional_headers)
 
         self.__base_client.checkResponse(
             response, f'Failed to create Signup.')
@@ -78,8 +76,8 @@ class Signups(object):
 
     def getSignupById(self,
                       namespace_id: str = None,
-                      community_id: str = None,
-                      signup_id: str = None) -> tuple[Signup, str]:
+                      signup_id: str = None,
+                      community_id: str = None) -> tuple[Signup, str]:
         """
         Retrieves a signup by signup identifier. 
 
@@ -87,13 +85,12 @@ class Signups(object):
         :param str signup_id: Signup Identifier.
         """
 
-        self.__base_client.validateParameters(namespace_id, signup_id)
+        self.__base_client.validateParameters(namespace_id, signup_id, community_id)
 
-        if community_id is not None:
-            communityHeader = self.__base_client.getCommunityIdHeader(community_id)
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         response = self.__base_client.request('get', self.__signup_path.format(
-            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), additional_headers=communityHeader)
+            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), additional_headers=additional_headers)
         self.__base_client.checkResponse(
             response, f'Failed to get Signup, {signup_id}.')
 
@@ -104,6 +101,7 @@ class Signups(object):
     def updateSignup(self,
                      namespace_id: str = None,
                      signup_id: str = None,
+                     community_id: str = None,
                      body: UpdateSignupInput = None,) -> Signup:
         """
         Updates the properties (for example, name) of a signup. 
@@ -113,10 +111,12 @@ class Signups(object):
         :param UpdateSignupInput body: Signup input object to replace the existing signup's properties.
         """
 
-        self.__base_client.validateParameters(namespace_id, signup_id, body)
+        self.__base_client.validateParameters(namespace_id, signup_id, community_id, body)
+        
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         response = self.__base_client.request('put', self.__signup_path.format(
-            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), data=body.toJson())
+            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), data=body.toJson(), additional_headers=additional_headers)
         self.__base_client.checkResponse(
             response, f'Failed to update Signup, {signup_id}.')
 
@@ -125,7 +125,8 @@ class Signups(object):
 
     def deleteSignup(self,
                      namespace_id: str = None,
-                     signup_id: str = None):
+                     signup_id: str = None,
+                     community_id: str = None):
         """
         Deletes a signup and related resources. 
 
@@ -133,26 +134,31 @@ class Signups(object):
         :param str signup_id: Signup unique identifier
         """
 
-        self.__base_client.validateParameters(namespace_id, signup_id)
+        self.__base_client.validateParameters(namespace_id, signup_id, community_id)
+
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         response = self.__base_client.request('delete', self.__signup_path.format(
-            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)))
+            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)),additional_headers=additional_headers)
         self.__base_client.checkResponse(
             response, f'Failed to delete Signup, {signup_id}.')
 
     def getSignupOwner(self,
                        namespace_id: str = None,
-                       signup_id: str = None) -> Trustee:
+                       signup_id: str = None,
+                       community_id: str = None) -> Trustee:
         """
         Retrieves the trustee (owner) of a signup. 
 
         :param str namespace_id: id of namespace to work against
         :param str signup_id: Signup unique identifier
         """
-        self.__base_client.validateParameters(namespace_id, signup_id)
+        self.__base_client.validateParameters(namespace_id, signup_id, community_id)
+        
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         response = self.__base_client.request('get', self.__signup_owner_path.format(
-            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)))
+            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), additional_headers=additional_headers)
         self.__base_client.checkResponse(
             response, f'Failed to get Signup owner, {signup_id}.')
 
@@ -161,17 +167,20 @@ class Signups(object):
 
     def getSignupResources(self,
                            namespace_id: str = None,
-                           signup_id: str = None) -> SignupResources:
+                           signup_id: str = None,
+                           community_id: str = None) -> SignupResources:
         """
         Retrieves a model that contains collections of accessible and inaccessible resources for a signup. 
 
         :param str namespace_id: id of namespace to work against
         :param str signup_id: Signup unique identifier
         """
-        self.__base_client.validateParameters(namespace_id, signup_id)
+        self.__base_client.validateParameters(namespace_id, signup_id, community_id)
+        
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         response = self.__base_client.request('get', self.__signup_resources_path.format(
-            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)))
+            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), additional_headers=additional_headers)
         self.__base_client.checkResponse(
             response, f'Failed to get Signup resources, {signup_id}.')
 
@@ -193,21 +202,20 @@ class Signups(object):
         :param SignupResourcesInput body: Signup resources input object to replace signup's resources.
         """
 
-        self.__base_client.validateParameters(namespace_id, community_id, signup_id, body)
+        self.__base_client.validateParameters(namespace_id, signup_id, community_id, body)
 
-        if community_id is not None:
-            communityHeader = self.__base_client.getCommunityIdHeader(community_id)
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         response = self.__base_client.request('post', self.__signup_resources_path.format(
-            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), data=body.toJson(), additional_headers=communityHeader)
+            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), data=body.toJson(), additional_headers=additional_headers)
         self.__base_client.checkResponse(
             response, f'Failed to get Signup resources, {signup_id}.')
 
     def getUpdates(self,
-                   namespace_id: str = None,
-                   community_id: str = None,
-                   signup_id: str = None,
-                   bookmark: str = None ) -> tuple[list[Update], str]:
+                    namespace_id: str = None,
+                    signup_id: str = None,
+                    community_id: str = None,
+                    bookmark: str = None ) -> tuple[list[Update], str]:
         """
         Returns a sequence of updates for all resources within the Signup, starting from the sequential marker represented by a provided `Bookmark`. 
 
@@ -217,17 +225,16 @@ class Signups(object):
         """
 
         self.__base_client.validateParameters(
-            namespace_id, community_id, signup_id)
+            namespace_id, signup_id, community_id)
         
-        if community_id is not None:
-            communityHeader = self.__base_client.getCommunityIdHeader(community_id)
+        additional_headers = BaseClient.getCommunityIdHeader(community_id)
 
         params = {}
         if bookmark is not None:
             params['bookmark'] = bookmark
 
         response = self.__base_client.request('get', self.__signup_updates_path.format(
-            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), additional_headers=communityHeader, params=params)
+            namespace_id=namespace_id, signup_id=self.__base_client.encode(signup_id)), additional_headers=additional_headers, params=params)
         self.__base_client.checkResponse(
             response, f'Failed to get Signup updates, {signup_id}.')
 
